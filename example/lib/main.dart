d394f2090037f37da841e31cc9ef8932007ef7c4
bekannax <bekannax@gmail.com>
2025-09-29 16:32:01 +0700

    update

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,38 +2,55 @@ import 'package:flutter/material.dart';
 import 'package:onlinechatsdk/chat_view.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
+  late ChatView chat;
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    String newCss = ".widget-header.draggable {" +
-        "display: none !important;" +
-        "}";
+    String newCss = ".widget-header.draggable { display: none !important; }";
+
+    // newCss = ".widget-header{display: none !important;}";
+
+    // newCss = "div.widget-header.widget-header-color{pointer-events: none !important;opacity: 0 !important; height: 0 !important;padding-top: 0 !important;padding-bottom: 0 !important;}";
+    newCss = "div.widget-header{display: none !important;}";
+
+    // newCss = ".online-chat-root .widget-header-background { background-image: linear-gradient(90deg, #00c7c4, #000ea8) !important; }";
+    // newCss = "div.onlineChatTextarea{ background: none !important; } .chatTab, div.chat-history-wrapper{background: url(https://maximumwallhd.com/wp-content/uploads/2015/07/fonds-ecran-ile-paradisique-15.jpg) !important; background-size: cover !important;}";
+
+    newCss = "";
+
+    chat = ChatView(
+      id: "593adecd804fc4e32e7e865d659f2356",
+      domain: "demo.ru",
+      language: "ru",
+      clientId: "",
+      apiToken: "",
+      css: newCss,
+      onOperatorSendMessage: (String data) {},
+      onClientSendMessage: (String data) {},
+      onClientMakeSubscribe: (String data) {},
+      onContactsUpdated: (String data) {},
+      onSendRate: (String data) {},
+      onClientId: (String data) {},
+      onCloseSupport: () {},
+      onFullyLoaded: (String data) {
+        // chat.injectCss(newCss);
+        chat.callJsSetClientInfo("{name: \"Имя\", email: \"test@mail.ru\"}");
+        // chat.callJsSetClientInfo("{name: \"${it.name}\", email: \"${it.email}\", phone: \"${it.phone}\"}")
+      },
+      safeArea: true,
+    );
 
     return MaterialApp(
       title: 'OnlineChatSdk-Flutter Demo',
-      home: ChatView(
-        id: "593adecd804fc4e32e7e865d659f2356",
-        domain: "sms-tx.ru",
-        language: "ru",
-        clientId: "",
-        apiToken: "",
-        css: newCss,
-        onOperatorSendMessage: (String data) {},
-        onClientSendMessage: (String data) {},
-        onClientMakeSubscribe: (String data) {},
-        onContactsUpdated: (String data) {},
-        onSendRate: (String data) {},
-        onClientId: (String data) {},
-        onCloseSupport: () {},
-        onFullyLoaded: (String data) {},
-      ),
+      color: Colors.white,
+      home: chat,
     );
   }
 }
